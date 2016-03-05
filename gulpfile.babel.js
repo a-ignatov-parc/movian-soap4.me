7e5e0d4121bfb494d8c38b0a99cc1003969474f5
Anton Ignatov <abietis@gmail.com>
2016-03-06 02:09:51 +0300

    Added config + plugin.json build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,44 @@
 import gulp from 'gulp';
+import assign from 'lodash/extend';
+import source from 'vinyl-source-stream';
 
-import config from './package.json';
+import config, {movian} from './package.json';
 
+const ASSETS = './assets';
 const SOURCE = './src';
 const DEST = './out';
 
-gulp.task('build', () => {
+const pluginFileName = 'index.js';
+
+const predefinedProps = {
+	file: pluginFileName
+};
+
+gulp.task('plugin', () => {
 	return gulp
 		.src(`${SOURCE}/**/*.js`)
 		.pipe(gulp.dest(DEST));
 });
 
-gulp.task('default', ['build']);
+gulp.task('assets', () => {
+	return gulp
+		.src(`${ASSETS}/**/*.{png,jpg,gif}`)
+		.pipe(gulp.dest(DEST));
+});
+
+gulp.task('config', () => {
+	let stream = source('plugin.json');
+	let configProps = Object
+		.keys(movian)
+		.filter((name) => movian[name] === null)
+		.reduce((result, name) => {
+			result[name] = predefinedProps[name] || config[name];
+			return result;
+		}, {});
+
+	stream.end(JSON.stringify(assign({}, movian, configProps)));
+
+	return stream.pipe(gulp.dest(DEST));
+});
+
+gulp.task('default', ['plugin', 'assets', 'config']);
